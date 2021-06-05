0cc95a853fea0c882961532ac3867fd3d657fc1f
meena108 <meena.hgmd@gmail.com>
2021-06-05 16:02:01 +0545

    Animation cat done with box animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,162 @@
+import 'dart:math' as Math;
+import 'package:animations/widgets/cat.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+
 
 void main() {
+  debugPaintSizeEnabled = false;
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animation',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Animations'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  Future<void> _incrementCounter() async {}
-  Animation<double> animation;
-  AnimationController controller;
+  late AnimationController catController =
+  AnimationController(duration: Duration(seconds: 3), vsync: this);
 
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(duration: Duration(seconds: 1), vsync: this);
-    animation = CurvedAnimation(curve: Curves.easeInOut, parent: controller);
-    controller.repeat();
-  }
+  late Animation<double> catAnimation =
+  Tween<double> (begin: -45, end: -80).animate(
+      CurvedAnimation(
+        curve: Curves.easeInOut,
+        parent: catController,
+        reverseCurve: Curves.easeIn,
+      )
+  );
+  late AnimationController handController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 400),
+  )..forward();
+  late Animation<double> handAnimation = Tween<double>(begin: Math.pi * 2/3, end: Math.pi * 3/4)
+      .animate(CurvedAnimation(parent: handController, curve: Curves.bounceIn),)
+    ..addStatusListener((status) {
+      if(status == AnimationStatus.dismissed) {
+        handController.forward();
+      }else if(status == AnimationStatus.completed){
+        handController.reverse();
+      }
+    });
 
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   catController!.forward();
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FadeTransition(
-              opacity: Tween<double>(begin: 4, end: 1).animate(animation),
-              child: RotationTransition(
-                turns: Tween<double>(begin: 0, end: 44 / 7).animate(animation),
-                child: ScaleTransition(
-                  alignment: Alignment.center,
-                  child: Container(width: 100, height: 100, color: Colors.red),
-                  scale: Tween<double>(begin: 1, end: 3).animate(animation),
-                ),
-              ),
-            )
-          ],
+        child: InkWell(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              _buildCatAnimation(),
+              _buildBox(),
+              _buildLeftHand(),
+              _buildRightHand(),
+            ],
+          ),
+          onTap: onTap,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    );
+  }
+
+  Widget _buildBox() {
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.brown.shade200,
+    );
+  }
+  Widget _buildCatAnimation(){
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (BuildContext context, Widget? child){
+        return Positioned(
+          top: catAnimation.value,
+          left: 0,
+          right: 0,
+          child: child!,
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  void onTap() {
+    if(catAnimation.status == AnimationStatus.dismissed ||
+        catAnimation.status == AnimationStatus.reverse) {
+      catController.forward();
+      handController.stop();
+    }else if(catAnimation.status == AnimationStatus.completed ||
+        catAnimation.status == AnimationStatus.forward ){
+      catController.reverse();
+      // handController.reset();
+
+    }
+  }
+  Widget _buildLeftHand() {
+    return Positioned(
+      left: 5,
+      top: 5,
+      child: AnimatedBuilder(
+        animation: handAnimation,
+        builder: (context, Widget? child) {
+          return Transform.rotate(
+            angle: handAnimation.value,
+            alignment: Alignment.topLeft,
+            origin: Offset(0,0),
+            child:child,
+          );
+        },
+        child: Container(
+          width: 100,
+          height: 18,
+          color: Colors.brown.shade200,
+        ),
+      ),
+    );
+  }
+  Widget _buildRightHand() {
+    return Positioned(
+      right: 6,
+      top: 4,
+      child: AnimatedBuilder(
+        animation: handAnimation,
+        builder: (context, Widget? child) {
+          return Transform.rotate(
+            angle: -handAnimation.value,
+            alignment: Alignment.topRight,
+            child:child,
+          );
+        },
+        child: Container(
+          width: 100,
+          height: 19,
+          color: Colors.brown.shade200,
+        ),
       ),
     );
   }
---
+++ lib/widgets/cat.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget {
+  const Cat ({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/images/Cat.png");
+  }
+}
